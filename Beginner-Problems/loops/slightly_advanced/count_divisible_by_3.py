3a7597fb2c54c7b9fc1007ae10199fdac753a905
Nitesh <niteshkjaiswal108@gmail.com>
2026-06-18 14:10:15 +0530

    Refactor count_divisible_by_3.py to use a counter instead of a list for total count of numbers divisible by 3, improving memory efficiency.

--- Beginner-Problems/loops/slightly_advanced/count_divisible_by_3.py
+++ Beginner-Problems/loops/slightly_advanced/count_divisible_by_3.py
@@ -1,11 +1,11 @@
 # Count how many numbers between 1 to N are divisible by 3.
 
 n_number = int(input('Enter A Number: '))
-total = []
+total = 0
 
 for numbers in range(1, n_number+1):
     if numbers % 3 == 0:
         print(numbers)
-        total.append(numbers)
+        total += 1
 
-print(f"Total: {len(total)}")
+print(f"Total: {total}")
