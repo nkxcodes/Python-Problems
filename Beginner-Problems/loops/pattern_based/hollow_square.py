4e543d395b89bf26569d273c44c0689cd9889b8c
Nitesh <niteshkjaiswal108@gmail.com>
2026-06-21 16:00:55 +0530

    Add a second solution to hollow_square.py for printing a hollow square pattern, enhancing loop practice with an alternative approach.

--- Beginner-Problems/loops/pattern_based/hollow_square.py
+++ Beginner-Problems/loops/pattern_based/hollow_square.py
@@ -16,3 +16,11 @@ for rows in range(1, 6):
         print('*   *')
     elif rows == 1 or 5:
         print('*' * 5)
+
+# Solution - 2
+
+for rows in range(1, 2):
+    print('*' * 5)
+    for int_rows in range(1, 4):
+        print('*   *')
+    print('*' * 5)
